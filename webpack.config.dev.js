3dd890f93246be6ae1b4de6e1d4507d911e367ed
Xu-Ning Li <xu-ning.li@osthus.com>
2017-12-08 10:59:54 +0100

    modify change

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -7,67 +7,61 @@ const port = process.env.PORT || 3000;
 const outputPath = path.join(__dirname, "dist");
 
 module.exports = {
-	entry: './src/app.jsx',
-	output: {
-		path: __dirname,
-		filename: 'dist/bundle.js'
-	},
-	resolve: {
-		modules: ['node_modules', './src'],
-		extensions: ['.js', '.jsx'],
-	},
-	module: {
-		loaders: [
-			{
-				test: /\.scss$/,
-				use: ExtractTextPlugin.extract({
-					use: 'css-loader!postcss-loader!sass-loader'
-				}),
-			},
-			// {
-			// 	test: /\.less$/,
-			// 	use: ExtractTextPlugin.extract({
-			// 		use: 'style-loader!css-loader!less-loader'
-			// 	}),
-			// },
-			{
-				test: /\.css$/,
-				use: ExtractTextPlugin.extract({
-					use: 'css-loader!postcss-loader'
-				}),
-			},
-			{
-				test: /\.(js|jsx)$/,
-				loader: 'babel-loader',
-				exclude: /node_modules/,
-				query: {
-					presets: ['es2015', "stage-0", "react"]
-				}
-			},
-			{
-				test: /\.jsx?$/,
-				exclude: /node_modules/,
-				loader: 'eslint-loader'
-			},
-			{
-				test: /\.(png|woff|woff2|eot|ttf|svg)$/,
-				loader: 'url-loader?limit=100000'
-			}
-		]
-	},
-	plugins: [
-		new ExtractTextPlugin("bundle.css"),
-		new webpack.HotModuleReplacementPlugin(),
-		new HtmlWebpackPlugin({
-			inject: true,
-			template: path.join(__dirname, '/index.html')
-		}),
-		new webpack.EnvironmentPlugin({ NODE_ENV: 'development' })
-	],
-	devtool: "source-map",
-	devServer: {
-		port,
-		hot: true,
-		historyApiFallback: true
-	}
+  entry: './src/app.jsx',
+  output: {
+    path: __dirname,
+    filename: 'dist/bundle.js'
+  },
+  resolve: {
+    modules: ['node_modules', './src'],
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          use: 'css-loader!postcss-loader!sass-loader'
+        }),
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          use: 'css-loader!postcss-loader'
+        }),
+      },
+      {
+        test: /\.(js|jsx)$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', "stage-0", "react"]
+        }
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader'
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=100000'
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin("bundle.css"),
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: path.join(__dirname, '/index.html')
+    }),
+    new webpack.EnvironmentPlugin({ NODE_ENV: 'development' })
+  ],
+  devtool: "source-map",
+  devServer: {
+    port,
+    hot: true,
+    historyApiFallback: true
+  }
 }
